1f057af954d547c8b3a22efa53cb0669e4d5e464
bigdata <bigdata@DESKTOP-VQLCD09>
2020-04-29 10:47:47 +0900

    2020/04/29 자바 Ch01

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,17 @@
+package sub1;
+
+/*
+ * 날짜: 2020/04/29
+ * 이름: 김영민 
+ * 내용: 자바 HElloWorld 출력하기
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+
+	}
+	
+}
